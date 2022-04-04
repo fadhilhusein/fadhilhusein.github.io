caa7280f0f7e4525ffe23b6619be7f1ad4fe5c14
fadhilhusein <muhammad.fadhil.syahrian@gmail.com>
2022-04-04 10:13:25 +0700

    update4

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const arrow = document.getElementById("btn");
 const hamburger = document.querySelector('header .hamburger');
 const mobile_menu = document.querySelector('header ul');
 const menu_item = document.querySelectorAll('header ul li a');
+const header = document.querySelector('header');
 
 function imgSlider(any){
     document.querySelector('.breads').src = any;
@@ -18,6 +20,22 @@ var swiper = new Swiper('.swiper', {
     },
   });
 
+  document.addEventListener('scroll',()=>{
+    var scroll = window.scrollY;
+    if(scroll > 250){
+        arrow.style.opacity = "1";
+    }else{
+        arrow.style.opacity = "0";
+    }
+})
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "#3A3845";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
+})
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -29,3 +47,7 @@ menu_item.forEach((item) => {
         mobile_menu.classList.toggle('active');
     })
 })
+
+ScrollOut({
+    targets: ".textBx,.imageBx,.content"
+  });
