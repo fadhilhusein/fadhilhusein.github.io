b4f71953c1d7be3bb3797f0bbc3632c4a0f2d13e
fadhilhusein <muhammad.fadhil.syahrian@gmail.com>
2022-04-07 10:28:43 +0700

    update7

--- app2.js
+++ app2.js
@@ -4,6 +4,15 @@ const mobile_menu = document.querySelector('header ul');
 const menu_item = document.querySelectorAll('header ul li a');
 const header = document.querySelector('header');
 
+document.addEventListener('scroll',()=>{
+    var scroll = window.scrollY;
+    if(scroll > 250){
+        arrow.style.opacity = "1";
+    }else{
+        arrow.style.opacity = "0";
+    }
+})
+
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
